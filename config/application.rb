e119dd404c55dcecaf71e964ecd09461a39dc355
Eduardo Pinheiro <eduardopinheiro@rmacbook.local>
2017-10-10 18:38:20 -0300

    Changes for heroku

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module HealthSmartCity
   class Application < Rails::Application
+  	config.assets.initialize_on_precompile = false
   	# config.web_console.whitelisted_ips = '10.0.2.2'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
