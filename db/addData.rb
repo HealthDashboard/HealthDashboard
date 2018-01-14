6aeed6f98be163c07bfe9846d0551a648828cb4f
Eduardo Pinheiro <eduardopinheiro@rmacbook.local>
2018-01-14 12:18:02 -0200

    add data fix

--- db/addData.rb
+++ db/addData.rb
@@ -1,5 +1,58 @@
 require 'csv'
 
+
+AGE_GROUP = [ "TP_0A4", "TP_5A9", "TP_10A14", "TP_15A19", "TP_20A24", "TP_25A29", "TP_30A34",
+              "TP_35A39", "TP_40A44", "TP_45A49", "TP_50A54", "TP_55A59", "TP_60A64", "TP_65A69",
+              "TP_70A74", "TP_75A79", "TP_80A84", "TP_85A89", "TP_90A94", "TP_95A99", "TP_100OUMA"]
+
+def get_age_code age
+  if age >= 0 && age <= 4
+    return AGE_GROUP[0]
+  elsif age >= 5 && age <= 9
+    return AGE_GROUP[1]
+  elsif age >= 10 && age <= 14
+    return AGE_GROUP[2]
+  elsif age >= 15 && age <= 19
+    return AGE_GROUP[3]
+  elsif age >= 20 && age <= 24
+    return AGE_GROUP[4]
+  elsif age >= 25 && age <= 29
+    return AGE_GROUP[5]
+  elsif age >= 30 && age <= 34
+    return AGE_GROUP[6]
+  elsif age >= 35 && age <= 39
+    return AGE_GROUP[7]
+  elsif age >= 40 && age <= 44
+    return AGE_GROUP[8]
+  elsif age >= 45 && age <= 49
+    return AGE_GROUP[9]
+  elsif age >= 50 && age <= 54
+    return AGE_GROUP[10]
+  elsif age >= 55 && age <= 59
+    return AGE_GROUP[11]
+  elsif age >= 60 && age <= 64
+    return AGE_GROUP[12]
+  elsif age >= 65 && age <= 69
+    return AGE_GROUP[13]
+  elsif age >= 70 && age <= 74
+    return AGE_GROUP[14]
+  elsif age >= 75 && age <= 79
+    return AGE_GROUP[15]
+  elsif age >= 80 && age <= 84
+    return AGE_GROUP[16]
+  elsif age >= 85 && age <= 89
+    return AGE_GROUP[17]
+  elsif age >= 90 && age <= 94
+    return AGE_GROUP[18]
+  elsif age >= 95 && age <= 99
+    return AGE_GROUP[19]
+  elsif age >= 100
+    return AGE_GROUP[20]
+  else
+    return AGE_GROUP[0]
+  end
+end
+
 def create_procedures()
   hc_csv_path = File.join(__dir__, "csv/procedures.csv")
   counter = 0
