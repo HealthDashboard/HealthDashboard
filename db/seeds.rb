be033c8cced12aef734cc335b0f44d323a6589b0
Eduardo Pinheiro <eduardopinheiro@rmacbook.local>
2018-01-14 16:40:10 -0200

    seed data fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -276,7 +276,7 @@ def create_procedures()
   counter = 0
   CSV.foreach(hc_csv_path, :headers => true) do |row|
     counter += 1
-    next if counter < 188802
+    next if counter < 217204
 
     age_code = get_age_code(row[4].to_i)
     spec = nil
