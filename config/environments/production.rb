cdb8ab331aaeb8143d9a614d320f031b82735278
Eduardo Pinheiro <eduardopinheiro@rmacbook.local>
2018-03-28 17:15:01 -0300

    Static assets fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # assets
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
