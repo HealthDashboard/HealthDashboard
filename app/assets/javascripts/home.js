98aaaea87176fc3f12dd9a493a4d418e1f751c46
kodaira <naomicamila@hotmail.com>
2018-08-06 09:50:55 -0300

    Slides com transição

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,22 +5,31 @@ function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slider-img");
-  // var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+  if (n > x.length) {
+    slideIndex = 1
   }
-  // for (i = 0; i < dots.length; i++) {
-  //    dots[i].className = dots[i].className.replace(" w3-white", "");
-  // }
-  x[slideIndex-1].style.display = "block";
-  // dots[slideIndex-1].className += " w3-white";
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    $(x[i]).removeClass("active");
+    $(x[i]).removeClass("right");
+    $(x[i]).removeClass("left");
+  }
+  $(x[slideIndex-1]).addClass("active");
+  if (slideIndex == 1) {
+    $(x[3]).addClass("right");
+  }
+  else {
+    $(x[slideIndex-2]).addClass("right");
+  }
+  if (slideIndex == 4) {
+    $(x[0]).addClass("left");
+  }
+  else {
+    $(x[slideIndex]).addClass("left");
+  }
 }
