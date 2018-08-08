79d812a2c013e338e4bae1364a2d63fd55d562aa
kodaira <naomicamila@hotmail.com>
2018-08-08 09:25:12 -0300

    Os dados são reais

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,33 @@
 var slideIndex = 1;
 showDivs(slideIndex);
+getData();
+
+function getData() {
+  $.getJSON("/distance_metric.json", data, function(result) {
+    sum = 0;
+    $.each(result,function(key, value) {
+        sum += parseInt(value, 10);
+    });
+    sum = sum.toLocaleString('pt-BR');
+    $("#procedure-data").html(sum);
+  });
+  $.getJSON("/points", data, function(result) {
+    $("#hospital-data").html(result.length);
+  });
+  $.getJSON("/specialties_procedure_distance_average", data, function(result) {
+    distances = Object.values(result);
+    nspec = distances.length;
+    med = 0;
+    for (var i = 0; i < nspec; i++) {
+      med += distances[i];
+    }
+   med = med/nspec;
+   med = Math.round(med * 100) / 100;
+   med = med.toLocaleString('pt-BR');
+   $("#spec-data").html(nspec);
+   $("#desloc-data").html(med + " km");
+  });
+}
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
