d984696a33d40604bd83424a99de8c2d81269ffa
Eduardo Pinheiro <eduardo.pinheiro482@hotmail.com>
2018-08-09 13:31:50 -0300

    Fix procedure position on distance calculation

--- app/assets/javascripts/procedure.js
+++ app/assets/javascripts/procedure.js
@@ -222,13 +222,13 @@ function markerOnClick(e) {
 
     //Only get the data the first time its clicked
     if (e.target.getPopup() === undefined) {
-        $.getJSON(procedure_info_path, function(result) {
-            cnes = result[0].cnes_id;
+        $.getJSON(procedure_info_path, function(procedure) {
+            cnes = procedure[0].cnes_id;
             $.getJSON("procedure/health_centres_procedure", {cnes: cnes.toString()}, function(hc_latlong) {
-                procedure_latlong = e.target.getLatLng();
+                // procedure_latlong = e.target.getLatLng();
                 path_distance_real = "http:\/\/router.project-osrm.org\/route\/v1\/driving\/" 
-                + hc_latlong[0].long + "," + hc_latlong[0].lat + ";" + procedure_latlong.lng + "," 
-                + procedure_latlong.lat + "?overview=false";
+                + hc_latlong[0].long + "," + hc_latlong[0].lat + ";" + procedure[0].long + "," 
+                + procedure[0].lat + "?overview=false";
                 
                 $.getJSON(path_distance_real, function(distance) { //get Real distance usign router.project-osrm.org
                     v_distance = parseFloat(distance.routes[0].distance);
@@ -236,12 +236,12 @@ function markerOnClick(e) {
                     console.log(distance)
 
                     text =  "<strong>Estabelecimento: </strong>" + health_centres_array[parseInt(cnes)] + "<br>";
-                    text += "<strong>Sexo: </strong>" + sexp_var[result[0].gender] + "<br>";
-                    text +=  "<strong>Idade: </strong>" + result[0].age_number + "<br>";
-                    text += "<strong>CID: </strong>" + cid_array[result[0].cid_primary] + "<br>";
-                    text += "<strong>CRS: </strong>" + result[0].CRS + "<br>";
-                    text += "<strong>Data: </strong>" + result[0].date + "<br>";
-                    text += "<strong>Distância: </strong>" + parseFloat(result[0].distance).toFixed(1).replace(".", ",") + " Km <br>";
+                    text += "<strong>Sexo: </strong>" + sexp_var[procedure[0].gender] + "<br>";
+                    text +=  "<strong>Idade: </strong>" + procedure[0].age_number + "<br>";
+                    text += "<strong>CID: </strong>" + cid_array[procedure[0].cid_primary] + "<br>";
+                    text += "<strong>CRS: </strong>" + procedure[0].CRS + "<br>";
+                    text += "<strong>Data: </strong>" + procedure[0].date + "<br>";
+                    text += "<strong>Distância: </strong>" + parseFloat(procedure[0].distance).toFixed(1).replace(".", ",") + " Km <br>";
                     text += "<strong>Distância viária: </strong>" + v_distance.toFixed(1).replace(".", ",") + " Km <br>";
 
                     e.target.bindPopup(text, {direction:'top', cnes: cnes});
