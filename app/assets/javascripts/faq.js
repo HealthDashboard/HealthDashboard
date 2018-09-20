233c0ffc66284fae071c089f53a9e494391a40ff
kodaira <naomicamila@hotmail.com>
2018-09-20 15:00:16 -0300

    Adding anchors to question section Signed-off-by: Camila Naomi <naomicamila@hotmail.com> Signed-off-by: Eduardo Pinheiro <eduardo.pinheiro482@hotmail.com>

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,8 +1,16 @@
+var currentPos;
+
+function inicialize () {
+  var position = $("#type1").offset().left + $("#type1").width()/2 - 37;
+  $("#triangle").css("transform", "translate(" + position + "px)");
+  currentPos = 1;
+}
+
 function changeFAQ1 () {
   var position = $("#type1").offset().left + $("#type1").width()/2 - 37;
   $("#triangle").css("transform", "translate(" + position + "px)");
   $("#question-container").css("transform", "translate(0px)");
-
+  currentPos = 1;
 }
 
 function changeFAQ2 () {
@@ -10,7 +18,7 @@ function changeFAQ2 () {
   $("#triangle").css("transform", "translate(" + position + "px)");
   var scroll = $("#viewer").width();
   $("#question-container").css("transform", "translate(-" + scroll + "px)");
-  console.log(scroll)
+  currentPos = 2;
 }
 
 function changeFAQ3 () {
@@ -18,4 +26,12 @@ function changeFAQ3 () {
   $("#triangle").css("transform", "translate(" + position + "px)");
   var scroll = $("#viewer").width();
   $("#question-container").css("transform", "translate(-" + scroll*2 + "px)");
+  currentPos = 3;
+}
+
+function jump(h){
+    var url = location.href;               //Save down the URL without hash.
+    location.href = "#"+h;
+    window.scrollTo(window.scrollX, window.scrollY - 50);
+    history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
 }
