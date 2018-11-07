4825a80248c2d33862f3525887e0b35882f7727d
kodaira <naomicamila@hotmail.com>
2018-11-07 15:25:35 -0200

    Making it run on terminal

--- spec/javascripts/map_menu_spec.js
+++ spec/javascripts/map_menu_spec.js
@@ -1,5 +1,7 @@
-describe(‘Foo’, function() {
-  it(“does something”, function() {
+// include app/assets/javascripts/map_menu.js
+//= require map_menu.js
+describe("Foo", function() {
+  it("does something", function() {
     expect(1 + 1).toBe(2);
   });
 });
